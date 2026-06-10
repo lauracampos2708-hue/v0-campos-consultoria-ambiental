3fc3990369b4f325a6b146d0cf9198ed2c78dfb2
lauracampos2708-hue <lauracampos2708@gmail.com>
2026-06-10 08:51:27 -0300

    Remove unnecessary empty line in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter, Playfair_Display } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
-
 import Script from 'next/script'
 import './globals.css'
 
